eb80129bead20ac52f94485c1b2286581c0802ee
Tywael <tywael@localhost.localdomain>
2023-02-28 12:06:56 +0100

    core: mariadb + nginx

---
+++ srcs/requirements/mariadb/scripts/entrypoint.sh
@@ -0,0 +1,22 @@
+cat .setup 2>/dev/null
+
+if [ $? -ne 0 ]; then
+
+    mariadb-install-db --dated=/var/lib/mysql \
+        --auth-root-authentication-method=normal
+
+    chown -R mysql:mysql /var/lib/mysql
+    chown -R mysql:mysql /run/mysqld
+
+    mysqld_safe --datadir=/var/lib/mysql &
+
+    while ! mysqladmin ping -h "mariadb" --silent; do
+        sleep 1
+    done
+    eval "echo \"$(cat /tmp/create_db.sql)\"" | mariadb
+    touch .setup
+else
+    echo "Database is already created"
+fi
+
+mysqld_safe --date=/var/lib/mysql
